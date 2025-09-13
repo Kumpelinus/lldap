c968a841350d2d54aca1e169e62be19fdaeefc5c
Kumpelinus <kumpelinus@jat.de>
2025-09-13 15:48:19 +0200

    Simplify try_jwt_authentication

--- server/src/graphql_server.rs
+++ server/src/graphql_server.rs
@@ -130,11 +130,9 @@ async fn try_jwt_authentication<Handler: BackendHandler + Clone>(
     inner_payload: &mut actix_web::dev::Payload,
     data: &AppState<Handler>,
 ) -> Result<ValidationResults, actix_web::Error> {
-    if let Ok(bearer) = BearerAuth::from_request(req, inner_payload).await {
-        check_if_token_is_valid(data, bearer.token())
-    } else {
-        Err(actix_web::error::ErrorUnauthorized("Authentication failed"))
-    }
+    BearerAuth::from_request(req, inner_payload).await.and_then(|bearer| {
+        Ok(check_if_token_is_valid(data, bearer.token()))
+    })?
 }
 
 async fn graphql_route<Handler: BackendHandler + Clone>(
